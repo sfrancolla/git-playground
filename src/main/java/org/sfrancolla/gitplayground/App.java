52cd626728d01d6903b4f48dfb44decd5873ba84
Steven Francolla <sfrancolla@gmail.com>
2010-03-24 21:18:24 -0400

    install maven to git-playground.

---
+++ src/main/java/org/sfrancolla/gitplayground/App.java
@@ -0,0 +1,13 @@
+package org.sfrancolla.gitplayground;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
